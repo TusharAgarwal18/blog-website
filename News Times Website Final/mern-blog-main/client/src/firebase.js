046f333f523cf9627c416fd1ca87233f18b7bf4b
Tushar Agarwal <72596996+TusharAgarwal18@users.noreply.github.com>
2024-06-23 18:16:56 +0530

    Add files via upload

---
+++ News Times Website Final/mern-blog-main/client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtw4_6_vY1UJdC99uIwZ6USEHJxvl6r4c",
+  authDomain: "newstimes-4b715.firebaseapp.com",
+  projectId: "newstimes-4b715",
+  storageBucket: "newstimes-4b715.appspot.com",
+  messagingSenderId: "508230712743",
+  appId: "1:508230712743:web:969ffa5d160208b6f8ca82",
+  // measurementId: "G-1T753N81EF"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ News Times Website Final/mern-blog-main/client/src/pages/About.jsx
@@ -0,0 +1,64 @@
+export default function About() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About News Times
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to News Times, your premier destination for the latest
+              news and updates. News Times is more than
+              just a news blog; it's a dynamic platform designed to keep you
+              informed and engaged with the stories that matter most. Whether
+              you're interested in politics, technology, culture, or beyond,
+              News Times has something for everyone.
+            </p>
+
+            <p>
+              Created by a team of talented students from Heritage Institute of
+              Technology, Kolkata as their final year project, News Times boasts
+              a range of innovative features to enhance your news reading
+              experience. From a comprehensive admin dashboard to ensure smooth
+              operations, to a robust cuss words filter to maintain a respectful
+              environment, we strive to provide a top-notch user experience.
+            </p>
+
+            <h1 className="text-3xl font font-semibold text-center my-7">
+            Meet the Team
+          </h1>
+          
+            <p>
+              
+              Tushar Agarwal: As a key member of our team, Tushar
+              brings his expertise in web development to the table. With a keen
+              eye for detail and a passion for innovation, he's instrumental in
+              bringing our vision to life. 
+              <br />
+              Abhijeet Kumar: Abhijeet's creativity
+              and problem-solving skills make him an invaluable asset to News
+              Times. Whether it's designing user interfaces or optimizing
+              website performance, he's always up for the challenge. 
+              <br />
+              Riya:
+              Riya's passion for journalism shines through in her work at News
+              Times. She brings her expertise in database and backend to the table. 
+              With a talent for storytelling and a commitment to
+              accuracy, she plays a crucial role in shaping our editorial
+              content.
+            </p>
+            <p>
+              Join us as we embark on this journey to deliver the latest news,
+              foster a supportive community, and empower you to stay informed in
+              an ever-changing world. Stay tuned to News Times for all your news
+              needs!
+              <br />
+              <br />
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
